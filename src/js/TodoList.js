463ee090780268bed89f32d295d4dac1ec6f1b6e
Fernando Brandao <fwbrandao@gmail.com>
2018-11-19 08:07:51 +0000

    added filter list

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -3,7 +3,24 @@ import { observer } from "mobx-react";
 
 @observer
 export default class TodoList extends React.Component {
+  filter(e) {
+    this.props.store.filter = e.target.value
+  }
+
   render() {
-    return <h1>{this.props.store.todos[0]}</h1>
+    const { todos, filter, filteredTodos } = this.props.store
+
+    const todoLis = filteredTodos.map(todo => (
+      <li>{todo}</li>
+    ))
+    return <div>
+      <h1>Todos</h1>
+      <div>{filter}</div>
+      <input
+      className='filter'
+      value={filter}
+      onChange={this.filter.bind(this)}/>
+      <ul>{todoLis}</ul>
+    </div>
   }
 }
--- src/js/TodoStore.js
+++ src/js/TodoStore.js
@@ -1,10 +1,14 @@
-import { observable } from "mobx"
+import { computed, observable } from "mobx"
 
 export class TodoStore {
   @observable
   todos = ["eggs", "bacon"]
   @observable
   filter = ""
+  @computed get filteredTodos() {
+    var matchesFilter = new RegExp(this.filter, 'i')
+    return this.todos.filter(todo => !this.filter || matchesFilter.test(todo))
+  }
 }
 
 export default new TodoStore
